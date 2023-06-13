62f466679a85e25af7012dadb697548b44a6268d
truonghoanglong <57944514+truonghoanglong@users.noreply.github.com>
2023-06-13 23:57:42 +0700

    'render'

--- src/components/TableUsers.jsx
+++ src/components/TableUsers.jsx
@@ -1,5 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Container } from 'react-bootstrap'
+import Table from 'react-bootstrap/Table'
+import { fetchAllUser } from '../services/UserService'
 
 export const TableUsers = () => {
-    return <div>TableUsers</div>
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        getUsers()
+    }, [])
+
+    const getUsers = async () => {
+        let res = await fetchAllUser()
+        if (res && res.data) setData(res.data.data)
+    }
+
+    return (
+        <Container>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Email</th>
+                        <th>Frist Name</th>
+                        <th>Last Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data &&
+                        data.length > 0 &&
+                        data.map((item) => {
+                            return (
+                                <tr key={item.id}>
+                                    <td>{item.id}</td>
+                                    <td>{item.email}</td>
+                                    <td>{item.first_name}</td>
+                                    <td>{item.last_name}</td>
+                                </tr>
+                            )
+                        })}
+                </tbody>
+            </Table>
+        </Container>
+    )
 }
